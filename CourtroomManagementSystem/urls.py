20b42c2b2cd2b0432d0458e53ad39ba45dbb3709
Khooshrin Aspi Pithawalla <88196065+Khooshrin@users.noreply.github.com>
2022-04-25 19:38:22 +0530

    Add files via upload

---
+++ CourtroomManagementSystem/urls.py
@@ -0,0 +1,47 @@
+"""CourtroomManagementSystem URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from login.views import LoginClientAction, UserChoiceAction, LoginLawyerAction, LoginJudgeAction, LoginAdminAction
+from register.views import RegisterClientAction, RegisterJudgeAction, RegisterLawyerAction
+from dashboard.views import AdminDashboardAction, ClientDashboardAction, JudgeDashboardAction, LawyerDashboardAction
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',UserChoiceAction),
+    path('LoginAdmin',LoginAdminAction),
+    path('LoginAdmin#',LoginAdminAction),
+    path('LoginClient',LoginClientAction),
+    path('LoginJudge',LoginJudgeAction),
+    path('LoginLawyer',LoginLawyerAction),
+    path('LoginClient#',LoginClientAction),
+    path('LoginJudge#',LoginJudgeAction),
+    path('LoginLawyer#',LoginLawyerAction),
+    path('RegisterClient/',RegisterClientAction),
+    path('RegisterJudge/',RegisterJudgeAction),
+    path('RegisterLawyer/',RegisterLawyerAction),
+    path('RegisterClient/#',RegisterClientAction),
+    path('RegisterJudge/#',RegisterJudgeAction),
+    path('RegisterLawyer/#',RegisterLawyerAction),
+    path('RegisterClient/#',RegisterClientAction),
+    path('RegisterJudge/#',RegisterJudgeAction),
+    path('RegisterLawyer/#',RegisterLawyerAction),
+    path('AdminDashboard',AdminDashboardAction),
+    path('ClientDashboard',ClientDashboardAction),
+    path('LawyerDashboard',LawyerDashboardAction),
+    path('JudgeDashboard',JudgeDashboardAction),
+]
